448ff5c57307dd752425981323995c15fff2a55e
Zalexanninev15 <blue.shark@disroot.org>
2022-09-17 18:33:48 +0300

    Version 1.0.1
    Changes:
    - Added support for .NET Framework 4.8.1 and later (if released)
    - Slight edit

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::process;
 use std::process::Command;
 use try_catch::catch;
 
-mod frameworks;
+mod framework;
 mod windows;
 
 fn main() {
     winconsole::console::set_title("Universal .NET components checker").unwrap();
-    println!("Universal .NET components checker v1.0 by Zalexanninev15\nGitHub: https://github.com/Zalexanninev15/dotnet_checker\n");
+    println!("Universal .NET components checker v1.0.1 by Zalexanninev15\nGitHub: https://github.com/Zalexanninev15/dotnet_checker\n");
     if windows::is_app_elevated() {
         println!("Default version of .NET:");
         dotnet_check("--version");
@@ -29,7 +29,7 @@ fn main() {
         println!(".NET Framework 4.5 or later:");
         catch! {
             try {
-        frameworks::get_45_plus_from_registry();
+        framework::get_45_plus_from_registry();
             }
             catch _error {
                 println!(".NET Framework 4.5 or later is not detected!")
@@ -40,7 +40,7 @@ fn main() {
         println!(".NET Framework 1.0-4.0:");
         catch! {
             try {
-        frameworks::get_version_from_registry();
+        framework::get_version_from_registry();
             }
             catch _error {
                 println!(".NET Framework 1.0-4.0 is not detected!")
